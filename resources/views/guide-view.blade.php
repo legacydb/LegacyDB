29236caf46bf203536bf7eac238f1077be6a1deb
emotionalshaolin <harvey.seager@googlemail.com>
2017-01-03 16:12:02 +0000

    Changed styling & BG image

--- resources/views/guide-view.blade.php
+++ resources/views/guide-view.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-
 	<h1 class="post-title">{{ $guide->title }}</h1>
 	<p class="post-author">by {{ $guide->author }}</p>
-	@if($userIsAuthor)
-		<small>(<a href="/guide/edit/{{ $guide->id }}">edit</a>)</small>
-	@endif
-	
 	<hr/>
 	<div class="content">
 		@markdown($guide->content)
 	</div>
+	<hr/>
+	@if($userIsAuthor)
+		<a href="/guide/edit/{{ $guide->id }}" class="button button-primary">Edit Guide</a>
+		<a href="/guide/delete/{{ $guide->id }}" class="button">Delete</a>
+	@endif	
 	
 @endsection
